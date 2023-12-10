2124a08e89123d0a44f38694f95c2aabac841232
Masud-Ndatsu <masudndatsu@gmail.com>
2023-12-10 02:56:26 +0100

    feat: Updated user schema model

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -23,7 +23,27 @@ const schema = new Schema<IUser>({
      user_type: {
           type: String,
      },
-     shop: {
+     shop_name: {
+          type: String,
+          index: true,
+     },
+     password: {
+          type: String,
+          required: true,
+     },
+     address: {
+          type: String,
+     },
+     LGA: {
+          type: String,
+     },
+     auth_code: {
+          type: String,
+     },
+     auth_code_expires: {
+          type: Date,
+     },
+     reset_token: {
           type: String,
      },
 });
